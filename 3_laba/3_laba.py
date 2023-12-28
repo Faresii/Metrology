336d0f1b6d493c584f7146db4a8be9d483949768
Faresii <105324937+Faresii@users.noreply.github.com>
2023-12-28 14:14:05 +0300

    Update 3_laba.py

--- 3_laba/3_laba.py
+++ 3_laba/3_laba.py
@@ -1,37 +1,68 @@
 import pandas as pd
-import math as m
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-
-#filename = "C:\Project with Alexey\Metrology\11.xlsx"
-
+data = pd.read_excel('C:/ProjectWithAlexey/Metrology/3_laba/metrology/11.xlsx', skiprows=2, index_col=0)
+df = pd.DataFrame(data,
+                                            #Параметры импульса, при подключении коротким кабелем
+                                                #500                                    #100
+                     columns=['U, В','h, %', 'τ, нс', 'τф, нс', 'τс, нс', 'U, В','h, %', 'τ, нс', 'τф, нс', 'τс, нс', 
+                             'U, В','h, %', 'τ, нс', 'τф, нс', 'τс, нс',  'U, В','h, %', 'τ, нс', 'τф, нс', 'τс, нс',
+                                                #50                                        #20
+                                                
+                                            #Параметры импульса, при ограничении полосы пропускания осциллографа
+                                                #500                                    #100                                            
+                             'U, В','h, %', 'τ, нс', 'τф, нс', 'τс, нс', 'U, В','h, %', 'τ, нс', 'τф, нс', 'τс, нс', 
+                             'U, В','h, %', 'τ, нс', 'τф, нс', 'τс, нс',  'U, В','h, %', 'τ, нс', 'τф, нс', 'τс, нс'])
+                                                #50                                        #20
+                                                
+data1 = data
 P = 0.95
-W = {'U': 0}
 n = 20
+AgCri = 0 # agreement criterion - критерий согласия
 
-data = pd.read_excel('C:/ProjectWithAlexey/Metrology/3_laba/11.xlsx', skiprows=2, index_col=0)
-data_mean = pd.DataFrame(data.mean()).T.round(1)
-data_mean.index = ['mean']
-print(data_mean)
-#data.loc['mean'] = data_mean
-# data.loc['Ua'] = data.std() / m.sqrt(n)
-# data.loc['Ub'] = 0
+X_max = 0
 
-# data.loc['Ub']['U, В'] = W['U'] / m.sqrt(3)
-# for i in range(1, 8):
-#   data.loc['Ub']['U, В.' + str(i)] = W['U'] / m.sqrt(3)
+# print(data)
 
-# data.loc['Ub']['h, %'] = (W['U'] / m.sqrt(3)) * 100
-# for i in range(1, 8):
-#   data.loc['Ub']['h, %.' + str(i)] = (W['U'] / m.sqrt(3)) * 100
+# print(data.mean())
 
-# data.loc['Uc'] = (data.loc['Ua'] ** 2 + data.loc['Ub'] ** 2) ** (1 / 2)
+AgCri = ( 1 / (n*data.std(ddof=0)) ) * (data - data.mean())
+
+# print('\nКритерий согласия')
+# print(AgCri)
 
 
-# k = 2
-# data.loc['U'] = k * data.loc['Uc']
+def GetIndex():
+    for i in data:
+        data[0][i].loc(f'{i}')
+    return data
 
+def IdentifyingMisses(): #Выявление промахов для критерия согласия
+    X_max = data.max()
+    data_mean = data.mean()
+    
+    # for i in data :
+    #     if X_max[i] > data_mean[i] : data[i].drop()
+    print(X_max)
+    return X_max
 
-# for i in range(len(data.columns)):
-#   index = data.columns[i]
-#   tmp_series = data[index]
-#   print(f"U for {data.columns[i]}:\n {tmp_series['mean']},\t{tmp_series['U']},\t{P}\n")
+def main():
+    print('Выявление промахов')
+    IdentifyingMisses()
+
+def AllGraphs():
+    k = 0
+    for i in data:
+        
+        ax = plt.subplot(7, 7, k+1)
+        sns_plot = sns.distplot(data[i], norm_hist=True, label=None, rug=True)
+        k +=1
+    
+    fig = sns_plot.get_figure()
+    plt.show()
+    
+if __name__ == '__main__' :
+    main()
+    
+    AllGraphs()
